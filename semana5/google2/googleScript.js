d7387ccdae28935f4aaf7e75d3d4f5b29d872d18
MaFerLlamas <fernandaavilallamas@gmail.com>
2018-10-10 21:32:55 -0500

    se agrego la funcionaldad de que al poner espacios al buscador poder detectarlos y procesarlos de manera que google realice la busqueda

--- semana5/google2/googleScript.js
+++ semana5/google2/googleScript.js
@@ -2,16 +2,22 @@
 let buscador = document.querySelector('input');
 let btnBuscar = document.querySelector('#btnBuscar');
 
+buscador.addEventListener('keyup',buscar);
 btnBuscar.addEventListener('click', function(){
-    let busqueda = buscador.value;
+    let busqueda = quitarEspacios(buscador.value);
     window.location.href = "https://www.google.com.mx/search?q=" + busqueda;
 });
-buscador.addEventListener('keyup',buscar);
+
+function quitarEspacios(){
+    let busqueda = buscador.value;
+    let final = busqueda.replace(" ","+");
+    return final;
+}
 
 function buscar(e){
-    let busqueda = buscador.value;
+    let busqueda = quitarEspacios(buscador.value);
     if (e.key === "Enter"){
+        //cambiar de pagina
         window.location.href = "https://www.google.com.mx/search?q=" + busqueda;
     }
 }
-//cambiar de pagina
